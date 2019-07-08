912f1ebdd8be882c4cb55e3aa8c48b3f3d3fcf7b
Miguel_Castro <mctiger23@gmail.com>
2019-07-08 10:36:46 -0500

    press enter on add input

--- Random Food Gen v2/scripts.js
+++ Random Food Gen v2/scripts.js
@@ -1,4 +1,4 @@
-var restoPlace = ["Portillos","Chick Fil-A", "MODS", "Jasons Deli","Panera","Noodle's and Company", ];
+var restoPlace = ["Portillos","Chick Fil-A", "MODS", "Jasons Deli","Panera","Noodle's and Company", "Shake Shack", "Bo-Bo's", ];
 var listText = "";
 // List pre-defined list//
 window.onload = function listOptions(){
@@ -8,26 +8,26 @@ window.onload = function listOptions(){
     }
     document.getElementById("options").innerHTML = listText;
 };
-function addResto () {
-  var  i;
-  var text = "";
-//Prompt to add a place //
-    do {
-        i = prompt("Enter a Place You Want to Eat:");
-        if (i === null || i === ""){
-            break;
-        }
-        else {
-            restoPlace.push(i);
-            document.getElementById("options").innerHTML += "<h4 class='p-1 d-inline-block'>" + i +"</h4>" +"<br>";
-        }
-    } while(1);
-    var l;
-    for (l =0; l < restoPlace.length; l++) {
-      text += "<li>" + restoPlace[l] + "</li>";
-    }
-    document.getElementById("list").innerHTML= text;
-}
+// function addResto () {
+//   var  i;
+//   var text = "";
+// //Prompt to add a place //
+//     do {
+//         i = prompt("Enter a Place You Want to Eat:");
+//         if (i === null || i === ""){
+//             break;
+//         }
+//         else {
+//             restoPlace.push(i);
+//             document.getElementById("options").innerHTML += "<h4 class='p-1 d-inline-block'>" + i +"</h4>" +"<br>";
+//         }
+//     } while(1);
+//     var l;
+//     for (l =0; l < restoPlace.length; l++) {
+//       text += "<li>" + restoPlace[l] + "</li>";
+//     }
+//     document.getElementById("list").innerHTML= text;
+// }
 
 
 
@@ -47,18 +47,24 @@ function randomResto (){
 
 function addResto2(){
   var e = document.getElementById("addOption").value;
-    restoPlace.push(e);
-    document.getElementById("options").innerHTML += "<h4 class='p-1 d-inline-block'>" + e +"</h4>" +"<br>";
-    document.getElementById("addOption").value = "";
+    if (e == ""){
+      alert("Invalid Input");
+    } else {
+      restoPlace.push(e);
+      document.getElementById("options").innerHTML += "<h4 class='p-1 d-inline-block'>" + e +"</h4>" +"<br>";
+      document.getElementById("addOption").value = "";
+
+    }
 }
 
 //press enter key on the search box NOT WORKING//
-
-input.addEventListener('keyup', function(e){
+var search = document.getElementById("addOption");
+search.addEventListener('keypress', function(e){
   if (e.keyCode === 13){
-    addResto2();
+    document.getElementById("addBtn").click();
   }
 });
+
 
 function removeAllOptions(){
   document.getElementById("options").innerHTML =null;
