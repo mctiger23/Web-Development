e8c64b4c8e61f79871463e4271293f82a65b6d03
Miguel Castro <miggyc@Miguels-MacBook-Pro-2.local>
2019-07-16 10:30:13 -0500

    added toggle button for CC-prototype side nav

--- CC-Prototype/scripts.js
+++ CC-Prototype/scripts.js
@@ -1,5 +1,19 @@
 
 //Side nav toggle options
-$("#addDoc").on("click",function() {
-  $("nav.sidebar").toggle();
-});
+// $("#sideNavBtn").on("click",function() {
+//   $("nav.sidebar").toggle();
+//   $("sidNavBtn").
+// });
+
+
+document.getElementById("sideNavBtn").addEventListener("click",function(){
+  var icon = document.getElementById("iconArrow");
+  if (icon.classList.contains("fa-arrow-alt-circle-left")){
+    icon.classList.remove("fa-arrow-alt-circle-left");
+    icon.classList.add("fa-arrow-alt-circle-right");
+  }
+  else if (icon.classList.contains("fa-arrow-alt-circle-right")){
+    icon.classList.remove("fa-arrow-alt-circle-right");
+    icon.classList.add("fa-arrow-alt-circle-left");
+  }
+  });
