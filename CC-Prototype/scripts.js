6c43ea976afd2cb0175ac235ce3d1ada22ab8b47
Miguel_Castro <mctiger23@gmail.com>
2019-07-18 11:33:26 -0500

    added modal for EDC button

--- CC-Prototype/scripts.js
+++ CC-Prototype/scripts.js
@@ -1,11 +1,14 @@
+var listEDCAdded = [500815];
 
-//Side nav toggle options
+
+
+
 // $("#sideNavBtn").on("click",function() {
 //   $("nav.sidebar").toggle();
 //   $("sidNavBtn").
 // });
 
-
+//Side nav toggle options
 document.getElementById("sideNavBtn").addEventListener("click",function(){
   var icon = document.getElementById("iconArrow");
   if (icon.classList.contains("fa-arrow-alt-circle-left")){
@@ -17,3 +20,16 @@ document.getElementById("sideNavBtn").addEventListener("click",function(){
     icon.classList.add("fa-arrow-alt-circle-left");
   }
   });
+
+document.getElementById("addEDCBtn").addEventListener("click",function(){
+  var edcAdded = document.getElementById("edcNum").value;
+  var isNum = isNaN(edcAdded) === true;
+  if (edcAdded === "" || isNum){
+    document.getElementById("edcNum").value = "";
+    document.getElementById("edcNum").setAttribute("placeholder", "Enter EDC");
+  } else {
+    var node = document.createElement("LI");
+    listEDCAdded.push(edcAdded);
+    document.getElementById("edcList").innerHTML += "<li>EDC <strong>" + edcAdded +"</strong></li>";
+    document.getElementById("edcNum").value = "";}
+});
